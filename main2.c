fa79955fda23fef83000636eeeb9327dfdd44c86
cbipok930 <alex_andr_6@mail.ru>
2022-02-10 04:30:57 +0000

    dim2

--- main2.c
+++ main2.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
-float arr[10000000];
+float arr[1000000000];
 int main() {
     clock_t begin = clock();
-    float step = M_2_PI/9999999;
+    float step = M_2_PI/999999999;
     float sum = 0;
 #pragma acc data copyin(arr) copyout(sum)
 #pragma acc kernels
     {
-    for(int i =0; i < 10000000; i++)
+    for(int i =0; i < 1000000000; i++)
         arr[i] = sin(step * i);
     clock_t end = clock();
     printf("%f\n", (float)((float)(end - begin)/1000));
 	
     begin = clock();
-    for (int i =0; i < 10000000; i++)
+    for (int i =0; i < 1000000000; i++)
         sum+= arr[i];
     end = clock();
     printf("%f, %f\n",sum ,(float)((float)(end - begin)/1000));
